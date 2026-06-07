cfcabb6ceb1780004b3c25e883f24f7275c2c72e
Andre-tfl-projects <andre@tankfarm.io>
2026-06-07 16:15:59 -0400

    Add files via upload

---
+++ sw[1].js
@@ -0,0 +1,27 @@
+const CACHE = 'propcodeai-v2';
+const ASSETS = ['/', '/index.html', '/js/app.js', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (new URL(e.request.url).pathname.startsWith('/api/')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (e.request.method === 'GET' && res.ok) {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+      }
+      return res;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
